bfa31fd96ea73b6fa930e2a3a68d9c5c094f7c2f
kingCoders60 <spworld30@gmail.com>
2025-04-30 17:46:41 +0530

    commit 3.

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -30,14 +30,34 @@ export const getMessages = async(req,res) => {
         console.log("Error in getMessage contoller: ",error.message);
         res.status(500).json({message:"Internal server error!"})
     }
-}
+};
 
 export const sendMessaga=async(req,res)=>{
     try {
         const {text,image}=req.body;
         const {id:receiverId}=req.params;
         const senderId=req.user._id
+
+        let imageUrl;
+        if(image){
+            const uploadResponse=await cloudinary.uploader.upload(image);
+            imageUrl=uploadResponse.secure_url;
+        }
+
+        const newMessage=new Message({
+            senderId,
+            receiverId,
+            text,
+            image:imageUrl,
+        });
+
+        await newMessage.save();
+
+        //todo: realtime funcitonality will be go here.
+
+        res.status(200).json({newMessage});
     } catch (error) {
-        
+        console.log("Error in sendMessages controller:",error.message);
+        res.status(500).json({error:"Internal server Error!"});
     }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,21 @@
-import express from "express";//first web framwwork..
+import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
-import {connectDB} from "./lib/db.js"
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT||5000;
+const PORT = process.env.PORT || 4000;
 
-app.use(express.json())
-app.use(cookieParser);
+app.use(express.json());
+app.use(cookieParser());  // ✅ Fixed issue
 
-app.use("/api/auth",authRoutes);
-app.use("/api/message",messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log(`Server is running at port: ${PORT}`);
