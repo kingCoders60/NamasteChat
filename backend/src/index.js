37d5161417d7a113f71102f4a50854252fe18f59
kingCoders60 <spworld30@gmail.com>
2025-05-04 02:58:36 +0530

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -10,7 +11,9 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import {app,server,io} from "./lib/socket.js"
 dotenv.config();
+
 const PORT = process.env.PORT || 4000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -18,6 +21,14 @@ app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    });
+}
 
 
 app.use(bodyParser.json({ limit: "1000mb" }));  // Supports larger JSON payloads
