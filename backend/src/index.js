953d09b515656c2a696bf03ed7134ae8def20e86
kingCoders60 <spworld30@gmail.com>
2025-05-04 03:58:24 +0530

    completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,45 +1,42 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 import path from "path";
+
+import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
-import { connectDB } from "./lib/db.js";
-import cors from "cors";
-import bodyParser from "body-parser";
-import {app,server,io} from "./lib/socket.js"
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-    });
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-
-app.use(bodyParser.json({ limit: "1000mb" }));  // Supports larger JSON payloads
-app.use(bodyParser.urlencoded({ limit: "1000mb", extended: true }));  // Supports larger form data
-connectDB()
-    .then(() => {
-        server.listen(PORT,"0.0.0.0", () => {
-            console.log(`Server is running at port: ${PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.error("Database connection failed", error);
-        process.exit(1);
-    });
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
