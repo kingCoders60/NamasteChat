18c7d31e9c157b4bd41543a040311026877805d4
kingCoders60 <spworld30@gmail.com>
2025-05-04 11:16:01 +0530

    completed

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup, login, logout, updateProfile, checkAuth } from "../controllers/auth.controller.js";
+import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
@@ -7,12 +7,9 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.put("/update-profile", protectRoute, updateProfile); // ✅ Uses middleware for auth
+
+router.put("/update-profile", protectRoute, updateProfile);
+
 router.get("/check", protectRoute, checkAuth);
-
-
-router.get("/user/:userId", (req, res) => { 
-  res.send(`User ID: ${req.params.userId}`);
-});
 
 export default router;
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,14 +1,12 @@
 import express from "express";
-import {protectRoute} from "../middleware/auth.middleware.js"
-import {getUserForSidebar,getMessages,sendMessages} from "../controllers/message.controller.js"
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
+router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
 
-
-router.get("/users",protectRoute,getUserForSidebar)
-router.get("/:id",protectRoute,getMessages)
-router.get("/send/:id",protectRoute,sendMessages);
-router.post("/send/:id", protectRoute, sendMessages);
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
